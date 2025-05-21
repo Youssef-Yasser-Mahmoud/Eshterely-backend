a64ed1c4c1bb7c7a9a54fd6a466987badf194367
Youssef <youssefyaser2020@gmail.com>
2025-05-21 22:15:47 +0300

    Add televisions route to the application

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,9 @@
 const speaker = require("../routes/speakers");
+const televisions = require("../routes/televisions");
 const express = require("express");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/api/speakers", speaker);
+  app.use("/api/televisions", televisions);
 };
