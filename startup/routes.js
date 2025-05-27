7ef50dfb22ae5badab64ac01a0eda0381edf2470
KareemEhab <kareem8002010@gmail.com>
2025-05-28 01:01:52 +0300

    Add orders endpoints to routes file

--- startup/routes.js
+++ startup/routes.js
@@ -5,6 +5,7 @@ const soundbars = require("../routes/soundbars");
 const user = require("../routes/users");
 const auth = require("../routes/auth");
 const cart = require("../routes/cart");
+const orders = require("../routes/orders");
 const express = require("express");
 
 module.exports = function (app) {
@@ -16,4 +17,5 @@ module.exports = function (app) {
   app.use("/api/headphones", headphones);
   app.use("/api/soundbars", soundbars);
   app.use("/api/cart", cart);
+  app.use("/api/orders", orders);
 };
