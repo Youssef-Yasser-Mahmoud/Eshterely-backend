7fef5c99ddf4e7104dd197e9f02d214f46479db8
Mandour <mahmoudmandour200015@gmail.com>
2025-05-29 02:06:26 +0300

    update order routes

--- routes/orders.js
+++ routes/orders.js
@@ -19,6 +19,7 @@ router.get("/", auth, async (req, res) => {
 router.get("/:id", auth, async (req, res) => {
   try {
     const order = await Order.findById(req.params.id);
+
     if (!order) {
       return res.status(404).json({ message: "Order not found" });
     }
@@ -33,6 +34,9 @@ router.get("/:id", auth, async (req, res) => {
 router.post("/", auth, async (req, res) => {
   try {
     const { items, total, shippingInfo } = req.body;
+    console.log("items", items);
+    console.log("total", total);
+    console.log("shippingInfo", shippingInfo);
 
     if (!shippingInfo) {
       return res
